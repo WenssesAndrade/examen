8f501ed3c732b80e3bfffec5234553e4c9b7e99c
Wensses A. L <wensses_al2001@hotmail.com>
2021-09-14 21:04:33 -0500

    Actualizados

--- automovil.sql
+++ automovil.sql
@@ -2,3 +2,8 @@ create table automovil(
 idauto varchar(50) primary key,
 Modelo varchar(225));
 
+insert into automovil (idauto,Modelo) values ('GOL 2020','VWCAR2020GOLST');
+insert into automovil (idauto,Modelo) values ('POLO 2020','VWCAR2020POLOAUT');
+insert into automovil (idauto,Modelo) values ('VENTO 2020','VWCAR2020VENTOST');
+insert into automovil (idauto,Modelo) values ('JETTA 2020','VWCAR2020JETTAGLI');
+
--- vendedores.sql
+++ vendedores.sql
@@ -5,4 +5,8 @@ create table Vendedores(
 idvendedor varchar(50) primary key,
 Nombre varchar(50));
 
-
+insert into Vendedores (idvendedor,Nombre) values ('VWEM01','DE ALBA VILLALOBOS LILIANA LIZBETHs');
+insert into Vendedores (idvendedor,Nombre) values ('VWEM02','FACIO ESPINOSA JUAN CARLOS');
+insert into Vendedores (idvendedor,Nombre) values ('VWEM03','GOMEZ CANO JOSE MIGUEL');
+insert into Vendedores (idvendedor,Nombre) values ('VWEM04','GOMEZ CASTAÑEDA MARIA FERNANDA');
+insert into Vendedores (idvendedor,Nombre) values ('VWEM05','GOMEZ LOPEZ RAFAEL');
